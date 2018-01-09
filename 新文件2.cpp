938e70778de7e85048c2a0d44428687420cfd2e4
wanglichien <35257045+wanglichien@users.noreply.github.com>
2018-01-09 18:31:09 +0800

    C++

---
+++ 新文件2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=0;
+	int b=0;
+	cin>>a>>b;
+	for(int j;j<=100;j++){
+		for(int i;i<=100;j++){
+			cout<<a<<"*"<<b<<"="<<a*b<<endl;
+		}
+	}
+} 
