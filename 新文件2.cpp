94f41ea8de486deb77b3f093296cffe9139d333c
wanglichien <35257045+wanglichien@users.noreply.github.com>
2018-01-09 18:41:37 +0800

    just monika

--- 新文件2.cpp
+++ 新文件2.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a=0;
-	int b=0;
-	cin>>a>>b;
-	for(int j;j<=100;j++){
-		for(int i;i<=100;j++){
-			cout<<a<<"*"<<b<<"="<<a*b<<endl;
-		}
-	}
+	for(int i;i<=10000000;i++){
+		cout<<"Just Monika!!!!!!!!!"<<endl;
+			}
 } 
